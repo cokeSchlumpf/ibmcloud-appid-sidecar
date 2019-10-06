56f57b07a67d292ade57b49c5f23b1546671173a
Michael Wellner <michael.wellner@de.ibm.com>
2019-10-06 05:58:03 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# IBM Cloud App ID Sidecar
+
+This is a quite simple Node App which can be used as a sidecar which handles authentication via IBM Cloud App ID for a setup within a container platform like Kubernetes or OpenShift.
+
+## Configuration
+
+The sample below shows how you can configure the sidecar in your deployment and configure the required settings for the sidecar.
+
+```yaml
+
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+const parser = require('body-parser');
+const express = require('express');
+const session = require('cookie-session');
+const proxy = require('http-proxy-stream');
+const WebAppStrategy = require("ibmcloud-appid").WebAppStrategy;
+const passport = require('passport');
+
+const CALLBACK_URL = '/ibm/cloud/appid/callback';
+const PROXY_URL = process.env.SIDECAR_PROXY_URL || 'http://localhost:8080';
+const PORT = process.env.SIDECAR_PORT || 3030;
+const SESSION_SECRET = process.env.SIDECAR_SESSION_SECRET || 'lorem_ipsum_c3po_42';
+
+const APPID_TENANTID = process.env.SIDECAR_APPID_TENANTID || '{tenantId}';
+const APPID_CLIENTID = process.env.SIDECAR_APPID_CLIENTID || '{clientId}';
+const APPID_SECRET = process.env.SIDECAR_APPID_SECRET || '{secret}';
+const APPID_OAUTH_SERVERURL = process.env.SIDECAR_APPID_OAUTH_SERVERURL || '{oauthServerUrl}';
+const APPID_APP_URL = process.env.SIDECAR_APP_URL || ('http://localhost:' + PORT);
+
+/*
+ * Configure express
+ */
+const app = express();
+
+app.set('trust proxy', 1);
+app.use(parser.urlencoded({ extended: true }));
+
+/*
+ * Passsport configuration for IBM Cloud App Id
+ */
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new WebAppStrategy({
+    tenantId: APPID_TENANTID,
+    clientId: APPID_CLIENTID,
+    secret: APPID_SECRET,
+    oauthServerUrl: APPID_OAUTH_SERVERURL,
+    redirectUri: APPID_APP_URL + CALLBACK_URL
+}));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+   
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+app.get(CALLBACK_URL, passport.authenticate(WebAppStrategy.STRATEGY_NAME));
+
+/*
+ * Routes of application
+ */
+app.get('/_userinfo', function (req, res) {
+    res.json(req.user);
+});
+
+app.use(passport.authenticate(WebAppStrategy.STRATEGY_NAME), function (req, res) {
+    proxy(req, { url: `${PROXY_URL}${req.originalUrl}` }, res);
+});
+
+/*
+ * Start server
+ */
+app.listen(PORT, function () {
+    console.log('IBM Cloud App ID sidecar listening on port ' + PORT + '!');
+});
